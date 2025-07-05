b879db4557a7b3abbdfc118db592a4d37895d7d8
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-07-05 15:54:20 +0200

    deploy on avalanche

--- script/DeployGyroConfigManager.s.sol
+++ script/DeployGyroConfigManager.s.sol
@@ -19,6 +19,8 @@ contract DeployGyroConfigManager is Deployment {
             gyroConfig = 0xaC89cc9d78BBAd7EB3a02601B4D65dAa1f908aA6;
         } else if (block.chainid == 1329) { // Sei
             gyroConfig = 0x194941B55555Afd751285B8b792C7538152DeAdd;
+        } else if (block.chainid == 43114) { // Avalanche
+            gyroConfig = 0x8A5eB9A5B726583a213c7e4de2403d2DfD42C8a6;
         // TODO more chains
         } else {
             revert("Unknown chain");
